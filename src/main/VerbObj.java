4925bdaa6407c160e0996a6ef6b972471eed1668
DominikBi <34425521+DominikBi@users.noreply.github.com>
2018-09-28 22:24:42 +0200

    adding

---
+++ src/main/VerbObj.java
@@ -0,0 +1,45 @@
+package main;
+
+
+public class VerbObj {
+
+
+    static String Spanish;
+    static String Deutsch;
+    static int DeutschVerbs = 0;
+    static int SpansichVerbs = 0;
+
+    public static int getSpansichVerbs() {
+        return SpansichVerbs;
+    }
+
+    public static void setSpansichVerbs(int spansichVerbs) {
+        SpansichVerbs = spansichVerbs;
+    }
+
+
+
+    public static int getDeutschVerbs() {
+        return DeutschVerbs;
+    }
+
+    public static void setDeutschVerbs(int deutschVerbs) {
+        DeutschVerbs = deutschVerbs;
+    }
+
+    public static String getSpanish() {
+        return Spanish;
+    }
+
+    public static void setSpanish(String spanish) {
+        Spanish = spanish;
+    }
+
+    public static String getDeutsch() {
+        return Deutsch;
+    }
+
+    public static void setDeutsch(String deutsch) {
+        Deutsch = deutsch;
+    }
+}
--- src/main/main.java
+++ src/main/main.java
@@ -5,39 +5,71 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class main  implements ActionListener{
+
+public class main{
+    String verb = "Verb";
+    JFrame jFrame = new JFrame();
+    JButton button = new JButton("Check");
+    JLabel jLabel = new JLabel(verb);
+    JTextField jTextField = new JTextField("Spielen Jugar");
+    char[] textInChars;
+    String text = "";
+
+
+
+    private String[] DeutschList = new String[VerbObj.getDeutschVerbs()];
+    public String[] SpanischList = new String[VerbObj.getSpansichVerbs()];
+
 
     public static void main(String[] args){
+
         main main = new main();
         main.Learning();
     }
-    public void Learning()
+    public void Learning() {
 
-    {
-        JFrame jFrame = new JFrame();
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                VerbObj.setDeutsch("Spielen");
+                VerbObj.setSpanish("Jugar");
+                text = jTextField.getSelectedText();
+                System.out.println(text);
+                String mid = "";
+                String Deutsch = "";
+                String Spanisch = "";
+                for(int i = 0; i<textInChars.length;i++){
+                    mid = textInChars[i] + mid;
+                if(mid == " " || mid == null) {
+                    String finalDeutsch =  Deutsch;
+                        break;
+
+                }
+                else{
+                    Deutsch = mid  + Deutsch;
+                }
+                }
+                System.out.println(Deutsch);
+            }
+        });
+        jFrame.setTitle("Spanisch Lernen");
+        jLabel.setSize(400,400);
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        JButton button = new JButton("Test");
         jFrame.setLayout(new BorderLayout());
         jFrame.setSize(jFrame.getMaximumSize());
+        jFrame.add(jLabel, BorderLayout.NORTH);
         jFrame.add(button, BorderLayout.SOUTH);
+        jFrame.add(jTextField, BorderLayout.NORTH);
         jFrame.setVisible(true);
     }
-    {
+    public void NewVerb(String Deutsch,String Spanish){
 
+        DeutschList[VerbObj.getDeutschVerbs()] = Deutsch;
+        VerbObj.setDeutschVerbs(VerbObj.getDeutschVerbs()+1);
+        SpanischList[VerbObj.getSpansichVerbs()] = Spanish;
+        VerbObj.setSpansichVerbs(VerbObj.getSpansichVerbs()+1);
 
-
-    }
-    public String[] VerbInput(String... Verbs) {
-        String[] VerbList =new String[Verbs.length];
-        for (int i = 0; i< Verbs.length;i++) {
-            Verbs[i] = VerbList[i];
-        }
-        return VerbList;
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
 
     }
 }
